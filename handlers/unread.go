c45582f1d3aa443a9910b2a513f7ac6ab6bfdab7
BouncyElf <17710281664@163.com>
2018-09-23 13:28:30 +0800

    refactor: remove param `count` from table `unread`

--- handlers/unread.go
+++ handlers/unread.go
@@ -20,17 +20,11 @@ func updateUnreadHandler(req *air.Request, res *air.Response) error {
 	uid := req.Params["uid"]
 	gid := req.Params["gid"]
 	lastMID := req.Params["last_mid"]
-	qtype := req.Params["qtype"]
 	unread := models.GetUnread(uid, gid)
 	if unread == nil {
 		unread = models.NewUnread(uid, gid, lastMID)
 	}
 	unread.LastMID = lastMID
-	if qtype == "add" {
-		unread.Count++
-	} else {
-		unread.Count = 0
-	}
 	go unread.Save()
 	return utils.Success(res, "")
 }
--- models/unread.go
+++ models/unread.go
@@ -9,7 +9,6 @@ type Unread struct {
 	UID     string `gorm:"column:uid" json:"uid"`
 	GID     string `gorm:"column:gid" json:"gid"`
 	LastMID string `gorm:"column:last_mid" json:"last_mid"`
-	Count   int    `gorm:"column:count" json:"count"`
 }
 
 func (Unread) TableName() string {
@@ -35,7 +34,6 @@ func NewUnread(uid, gid string, lastMID ...string) *Unread {
 		UID:     uid,
 		GID:     gid,
 		LastMID: mid,
-		Count:   0,
 	}
 }
 
